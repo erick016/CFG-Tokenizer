9e249acda0cfd918783153c7118c6215e6aeb438
erick016 <erick016@cougars.csusm.edu>
2018-01-05 13:33:18 -0800

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*                  COPYRIGHT 2016 JAMES ERICKSON                                         
+﻿/*                  COPYRIGHT 2018 JAMES ERICKSON                                         
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
